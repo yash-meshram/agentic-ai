6410cc40937442d6fbd9a4ef23eb573aece89f1f
Yash Meshram <yashacmeshram@gmail.com>
2025-06-14 19:34:13 +0530

    Add agent pattern implementations for deterministic story generation, homework assistance, and multilingual routing

--- agent_patterns/homework_agent.py
+++ agent_patterns/homework_agent.py
@@ -1,17 +1,29 @@
 from agents import Agent, Runner, GuardrailFunctionOutput, InputGuardrail
 from pydantic import BaseModel
+from langchain_google_genai import GoogleGenerativeAI
+import asyncio, nest_asyncio
+from dotenv import load_dotenv
+import os
+
+load_dotenv("../.env")
+
+# defining model
+model = "litellm/gemini/gemini-2.0-flash"
+# model = "litellm/groq/llama-3.1-8b-instant"
 
 # Building agents
 math_agent = Agent(
     name="Math Agent",
     handoff_description="Specialist agent for Math questions",
-    instructions="You provide help with the math problem. Provide your resoning at each step and provide examples."
+    instructions="You provide help with the math problem. Provide your resoning at each step and provide examples.",
+    model = model
 )
 
 history_agent = Agent(
     name = "History Agent",
     handoff_description="Specialist agent for History questions",
-    instructions="You provide help with historical queries. Explain important event and context clearly."
+    instructions="You provide help with historical queries. Explain important event and context clearly.",
+    model = model
 )
 
 # Building guardrails - which will decide if the process should move forward or not
@@ -23,6 +35,7 @@ class HomeworkOutput(BaseModel):
 guardrial_agent = Agent(
     name="Guardrail Check Agent",
     instructions="Check if the user is asking out the homework.",
+    model = model,
     output_type=HomeworkOutput
 )
 
@@ -43,10 +56,11 @@ async def guardrail_homework(ctx, agent, input_data):
 triage_agent = Agent(
     name="Triage Agent",
     instructions="You determine which agent to use based on user homework question.",
+    model = model,
     handoffs=[math_agent, history_agent],
-    input_guardrails = InputGuardrail(
-        guardrail_function = guardrail_homework
-    )
+    input_guardrails = [
+        InputGuardrail(guardrail_function = guardrail_homework)
+    ]
 )
 
 
@@ -54,8 +68,12 @@ triage_agent = Agent(
 async def main():
     result = await Runner.run(
         starting_agent=triage_agent,
-        input="What is the capital of delhi?"
+        input="homwork question: what is quadratic functions"
     )
     print(result)
     print("----------------------------------------")
     print(result.final_output)
+    
+if __name__ == "__main__":
+    nest_asyncio.apply()
+    asyncio.run(main())
--- agent_patterns/routing.py
+++ agent_patterns/routing.py
@@ -9,46 +9,59 @@
 from agents import Agent, Runner, TResponseInputItem, RawResponsesStreamEvent, trace
 from openai.types.responses import ResponseTextDeltaEvent, ResponseContentPartDoneEvent
 import uuid
-import asyncio
+import asyncio, nest_asyncio
+from dotenv import load_dotenv
+
+# load_dotenv("../.env")
+load_dotenv()
+
+# defining model
+model = "litellm/gemini/gemini-2.0-flash"
+# model = "litellm/groq/llama-3.1-8b-instant"
 
 # building agents
 english_agent = Agent(
     name="English Agent",
-    instructions="You only speak English"
+    instructions="You only speak English",
+    model = model
 )
 
 hindi_agent = Agent(
     name="Hindi Agent",
-    instructions="You only speak Hindi"
+    instructions="You only speak Hindi",
+    model = model
 )
 
 tamil_agent = Agent(
     name="Tamil Agent",
-    instructions="You only speak Tamil"
+    instructions="You only speak Tamil",
+    model = model
 )
 
 # Generating Triage agent which will decide which agent to use
 triage_agent = Agent(
     name="Triage Agent",
-    instructions="Based on the request select the appropriate agent."
+    instructions="Based on the request select the appropriate agent.",
+    handoffs=[english_agent, hindi_agent, tamil_agent],
+    model = model
 )
 
 async def main():
     conversation_id = str(uuid.uuid4().hex[:16])
 
     message = "Hi, we speak, English, Hindi and Tamil. How can we help you?"
-    agent = triage_agent()
+    agent = triage_agent
     inputs: list[TResponseInputItem] = [{"content": message, "role": "user"}]
 
     while True:
         with trace("Routing example", group_id = conversation_id):
-            result = await Runner.run(
+            result = Runner.run_streamed(
                 agent,
                 input = inputs
             )
 
             # Will print response (result) token by token
-            async for event in result.stream_event():
+            async for event in result.stream_events():
                 if not isinstance(event, RawResponsesStreamEvent):
                     continue    # ignore other response event but continue the loop
                 data = event.data
@@ -67,7 +80,7 @@ async def main():
 
             user_message = input("Enter a message:")                            # user can enter new message (conversation)
             inputs.append({"content": user_message, "role": "user"})            # adding the new user input message to inputs which will be pass to agent
-            agent = result.current_agent                                        # setting current agent as agent (rethink on it)
+            # agent = result.current_agent                                        # setting current agent as agent (rethink on it)
 
 if __name__ == "__main__":
     asyncio.run(main())
